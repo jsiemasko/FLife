eab9a98e8826488863417fccc9ed33a10a931e66
Jonathan Siemasko <jsiemasko@gmail.com>
2019-07-22 23:27:24 -0400

    Added App

---
+++ FLifeApp/MainForm.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace FLifeApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.displayBox = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.displayBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // displayBox
+            // 
+            this.displayBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.displayBox.Location = new System.Drawing.Point(0, 0);
+            this.displayBox.Name = "displayBox";
+            this.displayBox.Size = new System.Drawing.Size(549, 780);
+            this.displayBox.TabIndex = 0;
+            this.displayBox.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 780);
+            this.Controls.Add(this.displayBox);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.displayBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox displayBox;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ FLifeApp/MainForm.cs
@@ -0,0 +1,64 @@
+﻿using SkiaSharp;
+using System;
+using System.Drawing;
+using System.IO;
+using static FLife.Grid;
+using static FLife.File;
+using static FLife.Cell;
+using static FLife.Cell.State.CellState;
+using System.Windows.Forms;
+using static SkiaSharp.SKColors;
+using Microsoft.FSharp.Collections;
+using static SkiaSharp.SKPaintStyle;
+using System.Linq;
+
+namespace FLifeApp
+{
+    public partial class MainForm : Form
+    {
+        private static readonly Random _random = new Random();
+        private FSharpList<Cell.Cell> _grid = notQuiteRandom;
+        private int _xScale = 15;
+        private int _yScale = 15;
+        private int _strokeWidth = 12;
+
+        public MainForm()
+            => InitializeComponent();
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            _grid = nextGridState(_grid);
+            DisplayGrid(_grid);
+        }
+
+        private void DisplayGrid(FSharpList<Cell.Cell> grid)
+        {
+            var imageInfo = new SKImageInfo(this.Width, this.Height);
+            using (var surface = SKSurface.Create(imageInfo))
+            {
+                var canvas = surface.Canvas;
+                canvas.Clear(Black);
+                var colorPredefined = Red;
+
+                using (var paint = new SKPaint())
+                {
+                    paint.Color = Red;
+                    paint.IsAntialias = false;
+                    paint.StrokeWidth = _strokeWidth;
+                    paint.Style = Stroke;
+                    var liveCells = _grid.Where(cell => cell.State == Alive);
+                    foreach (var cell in liveCells)
+                    {
+                        canvas.DrawRect(Cell.cellX(cell) * _xScale, Cell.cellY(cell) * _yScale, 1, 1, paint);
+                    }
+                }
+                using (var image = surface.Snapshot())
+                using (var data = image.Encode(SKEncodedImageFormat.Png, 100))
+                using (var mStream = new MemoryStream(data.ToArray()))
+                {
+                    displayBox.Image = new Bitmap(mStream, false);
+                }
+            }
+        }
+    }
+}
